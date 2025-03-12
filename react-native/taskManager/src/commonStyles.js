942bc682b27926c9cdca5605650e856e09130085
bielizr <gabrielcallegari789@gmail.com>
2025-03-11 22:18:30 -0300

    cria o componente Task

---
+++ react-native/taskManager/src/commonStyles.js
@@ -0,0 +1,11 @@
+//import { Colors } from "react-native/Libraries/NewAppScreen";
+
+export default {
+    fontFamily: 'Lato_400Regular',
+    Colors: {
+        today: '#B13B44',
+        secondary: '#FFF',
+        mainText: '#222',
+        subText: '#555'
+    }
+}
